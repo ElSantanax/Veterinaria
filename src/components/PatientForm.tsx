31f3a8a5b643df23aa6e0c79e78c5c57f5df9ce1
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-27 17:50:48 -0500

    Agregar manejo de errores en el formulario de pacientes y componente de Error

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,13 +1,17 @@
 import { useForm } from 'react-hook-form'
+import Error from './Error';
 
 export default function PatientForm() {
 
-    const { register } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const registerPatient = () => {
+        console.log('Nuevo paciente');
+    }
 
     return (
         <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-
             <p className="text-lg mt-5 text-center mb-10">
                 Añade Pacientes y {''}
                 <span className="text-indigo-600 font-bold">Administralos</span>
@@ -16,6 +20,7 @@ export default function PatientForm() {
             <form
                 className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
                 noValidate
+                onSubmit={handleSubmit(registerPatient)}
             >
                 <div className="mb-5">
                     <label htmlFor="name" className="text-sm uppercase font-bold">
@@ -27,9 +32,18 @@ export default function PatientForm() {
                         type="text"
                         placeholder="Nombre del Paciente"
                         {...register('name', {
-                            required: 'El nombre del paciente es obligatorio'
+                            required: 'El nombre del paciente es obligatorio',
+                            // maxLength: {
+                            //     value: 15,
+                            //     message: 'Máximo 10 caracteres'
+                            // }
                         })}
                     />
+                    {errors.name && (
+                        <Error>
+                            {errors.name?.message?.toString()}
+                        </Error>
+                    )}
                 </div>
 
                 <div className="mb-5">
