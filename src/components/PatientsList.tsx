f235083a280df870fac6a30ee8a13cea6f0d6480
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-28 18:23:26 -0500

    Agregar uuid para la generación de IDs de pacientes y actualizar el estado en el formulario de pacientes

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,5 +1,9 @@
+import { usePatientState } from "../store"
 
 export default function PatientsList() {
+    const patients = usePatientState(state => state.patients)
+    console.log(patients);
+
     return (
         <div>PatientsList</div>
     )
--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,22 @@
 import { create } from "zustand";
 import type { DraftPatient, Patient } from "./types";
+import { v4 as uuidv4 } from 'uuid'
 
 type PatientState = {
     patients: Patient[]
     addPatient: (date: DraftPatient) => void
 }
 
-export const usePatientState = create<PatientState>(() => ({
+const createPatient = (patient: DraftPatient): Patient => {
+    return { ...patient, id: uuidv4() }
+}
+
+export const usePatientState = create<PatientState>((set) => ({
     patients: [],
     addPatient: (date) => {
-        console.log(date);
+        const newPatient = createPatient(date)
+        set((state) => ({
+            patients: [...state.patients, newPatient]
+        }))
     }
 }))
