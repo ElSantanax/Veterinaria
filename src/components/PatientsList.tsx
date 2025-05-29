9f0258432b38b11d3be38d3d280790ac40fa9e1e
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-29 18:26:36 -0500

    Agregar componente PatientDetails y mejorar la visualización de la lista de pacientes

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,10 +1,34 @@
 import { usePatientState } from "../store"
+import PatientDetails from "./PatientDetails"
 
 export default function PatientsList() {
-    const patients = usePatientState(state => state.patients)
-    console.log(patients);
+    const patients = usePatientState((state) => state.patients)
 
     return (
-        <div>PatientsList</div>
+        <div className="md:w-1/2 lg:h-3/5 md:h-screen">
+            {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administras tus {''}
+                        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                    </p>
+                    {patients.map(patient => (
+                        <PatientDetails
+                            key={patient.id}
+                            patient={patient}
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes {''}
+                        <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+                    </p>
+                </>
+            )}
+        </div>
     )
 }
