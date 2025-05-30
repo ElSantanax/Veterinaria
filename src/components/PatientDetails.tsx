5e437d19685829ecdaa0b8535f9292584d0f8c20
ElSantanax <josesantanaserrano130598@gmail.com>
2025-05-29 19:41:25 -0500

    Agregar componente PatientDetailsItems y mejorar la visualización de detalles del paciente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import type { Patient } from "../types"
+import PatientDetailsItems from "./PatientDetailsItems"
 
 type PatientDetailsProps = {
     patient: Patient
@@ -6,8 +7,13 @@ type PatientDetailsProps = {
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
     return (
-        <div>
-            {patient.name}
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+            <PatientDetailsItems label="ID" data={patient.id} />
+            <PatientDetailsItems label="Nombre" data={patient.name} />
+            <PatientDetailsItems label="Dueño" data={patient.caretaker} />
+            <PatientDetailsItems label="Correo" data={patient.email} />
+            <PatientDetailsItems label="Fecha de alta" data={patient.date.toString()} />
+            <PatientDetailsItems label="Sintoma" data={patient.symptoms} />
         </div>
     )
 }
--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -5,7 +5,7 @@ export default function PatientsList() {
     const patients = usePatientState((state) => state.patients)
 
     return (
-        <div className="md:w-1/2 lg:h-3/5 md:h-screen">
+        <div className="md:w-1/2 lg:h-3/5 md:h-screen overflow-y-scroll">
             {patients.length ? (
                 <>
                     <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
